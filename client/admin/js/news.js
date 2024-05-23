f29a592768fad2e4e2ed373570115f79c0b00463
Le Anh Duc <ducvui2003@gmail.com>
2024-05-23 12:42:52 +0700

    create obj news, category

--- client/admin/js/news.js
+++ client/admin/js/news.js
@@ -1,5 +1,5 @@
 import { CATEGORIES, NEWS } from "./const.js";
-import { News } from "./type.js";
+import { Category, News } from "./type.js";
 import { formatDate } from "./utils.js";
 $(".nav-item.dropdown").on("click", function () {
   $(".dropdown-menu").toggleClass("show");
@@ -7,6 +7,16 @@ $(".nav-item.dropdown").on("click", function () {
 });
 
 $(document).ready(function () {
+  $.validator.addMethod(
+    "urlCheck",
+    function (value, element) {
+      // Regular expression for URL validation
+      var urlRegex =
+        /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+      return this.optional(element) || urlRegex.test(value);
+    },
+    "Please enter a valid URL."
+  );
   // Load data table first
   $("#data").DataTable({
     ajax: {
@@ -42,13 +52,16 @@ $(document).ready(function () {
       },
     ],
     initComplete: function () {
-      modalDeleteBtn();
-      modalUpdateBtn();
-      modalCreate();
-      modalDelete();
-      modalUpdate();
-      setupForm("#form__add");
-      setupForm("#form__edit");
+      $("#btn__create").on("click", function () {
+        modalCreate();
+      });
+      // modalDeleteBtn();
+      // modalUpdateBtn();
+
+      // modalDelete();
+      // modalUpdate();
+
+      // setupForm("#form__edit");
     },
   });
   // save id want to delete
@@ -72,7 +85,6 @@ $(document).ready(function () {
 
   function modalCreate() {
     const modalCreate = $("#modal__create");
-    // Validate form
 
     modalCreate.find("#btn__delete").click(function () {
       $.ajax({
@@ -84,6 +96,7 @@ $(document).ready(function () {
         },
       });
     });
+    setupForm("#form__add");
   }
 
   function modalUpdate() {
@@ -114,169 +127,171 @@ $(document).ready(function () {
     });
   }
   function setupForm(formSelector, data) {
-    $.validator.addMethod(
-      "urlCheck",
-      function (value, element) {
-        // Regular expression for URL validation
-        var urlRegex =
-          /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
-        return this.optional(element) || urlRegex.test(value);
-      },
-      "Please enter a valid URL."
-    );
-
-    // Froala
     let editorContent = "";
-    const selectorEditor = `${formSelector} .content`;
-    const editor = new FroalaEditor(selectorEditor, {
-      events: {
-        contentChanged: function () {
-          editorContent = editor.html.get();
-        },
-      },
-    });
-    // Lấy thể loại
-    const selectCategory = `${formSelector} .select__category`;
-    $.ajax({
-      url: CATEGORIES,
-      type: "GET",
-      success: function (data) {
-        const selectOptions = data.map((item) => ({
-          value: item.id,
-          text: item.name,
-        }));
-        $(selectCategory).selectize({
-          plugins: ["restore_on_backspace", "clear_button"],
-          delimiter: " - ",
-          persist: false,
-          maxItems: null,
-          options: selectOptions,
-          valueField: "value",
-          labelField: "text",
-          searchField: "text",
-        });
-      },
-    });
+    const selectCategory = `${formSelector} select[name="category[]"]`;
 
-    // Apply validate form
-    $(formSelector).validate({
-      rules: {
-        author: {
-          required: true,
-          minlength: 5,
-        },
-        title: {
-          required: true,
-          minlength: 5,
-        },
+    getCategories();
+    configTextEditor();
+    validate();
 
-        source: {
-          required: true,
-          urlCheck: true,
-        },
-        "category[]": {
-          required: true,
-        },
-        shortDescription: {
-          required: true,
-          minlength: 100,
-        },
-        content: {
-          required: true,
-          minlength: 5,
-        },
-        thumbnail: {
-          required: true,
-          urlCheck: true,
-        },
-      },
-      messages: {
-        title: {
-          required: "Title is required",
-          minlength: "Title must be at least 5 characters",
-        },
-        author: {
-          required: "Author is required",
-          minlength: "Author must be at least 5 characters",
-        },
-        source: {
-          required: "Source is required",
-          urlCheck: "Please enter a valid URL",
-        },
-        "category[]": {
-          required: "Please select at least one category",
-        },
-        shortDescription: {
-          required: "Short description is required",
-          minlength: "Short description must be at least 100 characters",
-        },
-        content: {
-          required: "Content is required",
-          minlength: "Content must be at least 5 characters",
-        },
-        thumbnail: {
-          required: "Thumbnail is required",
-          urlCheck: "Please enter a valid URL",
-        },
-      },
-      validClass: "is-valid",
-      errorClass: "is-invalid",
-      errorPlacement: function (error, element) {
-        $(element).next().text(error.text());
-      },
-      highlight: function (element, errorClass, validClass) {
-        $(element)
-          .addClass(errorClass)
-          .removeClass(validClass)
-          .attr("required", "required");
-        $(element).next().addClass("invalid-feedback");
-      },
-      unhighlight: function (element, errorClass, validClass) {
-        $(element)
-          .removeClass(errorClass)
-          .addClass(validClass)
-          .removeAttr("required");
-        $(element).next().text("");
-      },
-      submitHandler: function (form) {
-        const formData = new FormData(form);
-        const jsonData = new News();
-        formData.forEach(function (value, key) {
-          jsonData[key] = value;
-        });
-        jsonData.createdAt = new Date().toISOString();
-        console.log(jsonData);
-        $.ajax({
-          url: NEWS,
-          type: "POST",
-          data: JSON.stringify(jsonData),
-          contentType: "application/json",
-          success: function (response) {
-            Swal.fire({
-              title: "Success!",
-              text: "News has been added successfully!",
-              icon: "success",
-            });
+    // configTextEditor
+    function configTextEditor() {
+      // Froala Editor
+      const selectorEditor = `${formSelector} .content`;
+      const editor = new FroalaEditor(selectorEditor, {
+        events: {
+          contentChanged: function () {
+            editorContent = editor.html.get();
           },
-          error: function (xhr, status, error) {
-            Swal.fire({
-              title: "Failed!",
-              text: "Something went wrong!",
-              icon: "error",
-              confirmButtonText: "OK",
-            }).then((result) => {
-              // Check if the "OK" button is clicked
-              if (result.isConfirmed) {
-                // Close the modal
-                Swal.close();
-                modalUpdate.hide();
-              }
-            });
+        },
+      });
+    }
+
+    // getCategories
+    function getCategories() {
+      $.ajax({
+        url: CATEGORIES,
+        type: "GET",
+        success: function (data) {
+          const selectOptions = data.map((item) => ({
+            value: item.id,
+            text: item.name,
+          }));
+          $(selectCategory).selectize({
+            plugins: ["restore_on_backspace", "clear_button"],
+            delimiter: " - ",
+            persist: false,
+            maxItems: null,
+            options: selectOptions,
+            valueField: "value",
+            labelField: "text",
+            searchField: "text",
+          });
+        },
+      });
+    }
+
+    // Validate
+    function validate() {
+      $(formSelector).validate({
+        rules: {
+          author: {
+            required: true,
+            minlength: 5,
           },
-        });
-        return false;
-      },
-    });
+          title: {
+            required: true,
+            minlength: 5,
+          },
+
+          source: {
+            required: true,
+            urlCheck: true,
+          },
+          "category[]": {
+            required: true,
+          },
+          shortDescription: {
+            required: true,
+            minlength: 100,
+          },
+          content: {
+            required: true,
+            minlength: 5,
+          },
+          thumbnail: {
+            required: true,
+            urlCheck: true,
+          },
+        },
+        messages: {
+          title: {
+            required: "Title is required",
+            minlength: "Title must be at least 5 characters",
+          },
+          author: {
+            required: "Author is required",
+            minlength: "Author must be at least 5 characters",
+          },
+          source: {
+            required: "Source is required",
+            urlCheck: "Please enter a valid URL",
+          },
+          "category[]": {
+            required: "Please select at least one category",
+          },
+          shortDescription: {
+            required: "Short description is required",
+            minlength: "Short description must be at least 100 characters",
+          },
+          content: {
+            required: "Content is required",
+            minlength: "Content must be at least 5 characters",
+          },
+          thumbnail: {
+            required: "Thumbnail is required",
+            urlCheck: "Please enter a valid URL",
+          },
+        },
+        validClass: "is-valid",
+        errorClass: "is-invalid",
+        errorPlacement: function (error, element) {
+          $(element).next().text(error.text());
+        },
+        highlight: function (element, errorClass, validClass) {
+          $(element)
+            .addClass(errorClass)
+            .removeClass(validClass)
+            .attr("required", "required");
+          $(element).next().addClass("invalid-feedback");
+        },
+        unhighlight: function (element, errorClass, validClass) {
+          $(element)
+            .removeClass(errorClass)
+            .addClass(validClass)
+            .removeAttr("required");
+          $(element).next().text("");
+        },
+        submitHandler: function (form) {
+          const formData = new FormData(form);
+          const news = new News();
+          formData.forEach(function (value, key) {
+            if (key == "category[]") {
+              const category = new Category(parseInt(value));
+              news.addCategory(category);
+            } else news[key] = value;
+          });
+          news.createdAt = new Date().toISOString();
+          $.ajax({
+            url: NEWS,
+            type: "POST",
+            data: JSON.stringify(news),
+            contentType: "application/json",
+            success: function (response) {
+              Swal.fire({
+                title: "Success!",
+                text: "News has been added successfully!",
+                icon: "success",
+              });
+            },
+            error: function (xhr, status, error) {
+              Swal.fire({
+                title: "Failed!",
+                text: "Something went wrong!",
+                icon: "error",
+                confirmButtonText: "OK",
+              }).then((result) => {
+                // Check if the "OK" button is clicked
+                if (result.isConfirmed) {
+                  // Close the modal
+                  Swal.close();
+                }
+              });
+            },
+          });
+        },
+      });
+    }
   }
   // const configFroala = {
   //   imageUploadParam: "image_param",
--- client/admin/js/type.js
+++ client/admin/js/type.js
@@ -1,96 +1,21 @@
 export class News {
-  #id;
-  #title;
-  #author;
-  #source;
-  #category;
-  #shortDescription;
-  #content;
-  #thumbnail;
+  constructor(title, author, source, shortDescription, content, thumbnail) {
+    this.title = title;
+    this.author = author;
+    this.source = source;
+    this.shortDescription = shortDescription;
+    this.content = content;
+    this.thumbnail = thumbnail;
+    this.category = [];
+  }
 
-  constructor(
-    title,
-    author,
-    source,
-    category,
-    shortDescription,
-    content,
-    thumbnail
-  ) {
-    this.#title = title;
-    this.#author = author;
-    this.#source = source;
-    this.#category = category;
-    this.#shortDescription = shortDescription;
-    this.#content = content;
-    this.#thumbnail = thumbnail;
-  }
-  get id() {
-    return this.#id;
-  }
-  get title() {
-    return this.#title;
-  }
-  get author() {
-    return this.#author;
-  }
-  get source() {
-    return this.#source;
-  }
-  get category() {
-    return this.#category;
-  }
-  get shortDescription() {
-    return this.#shortDescription;
-  }
-  get content() {
-    return this.#content;
-  }
-  get thumbnail() {
-    return this.#thumbnail;
-  }
-  set title(title) {
-    this.#title = title;
-  }
-  set author(author) {
-    this.#author = author;
-  }
-  set source(source) {
-    this.#source = source;
-  }
-  set category(category) {
-    this.#category = category;
-  }
-  set shortDescription(shortDescription) {
-    this.#shortDescription = shortDescription;
-  }
-  set content(content) {
-    this.#content = content;
-  }
-  set thumbnail(thumbnail) {
-    this.#thumbnail = thumbnail;
-  }
-  set id(id) {
-    this.#id = id;
+  addCategory(category) {
+    this.category.push(category.id);
   }
 }
 
 export class Category {
-  #id;
-  #name;
-  constructor(name) {
-    this.#name = name;
-  }
-  get id() {
-    return this.#id;
-  }
-  get name() {
-    return this.#name;
-  }
-  set id(id) {
-    this.#id = id;
-  }
-  set name(name) {
-    this.#name = name;
+  constructor(id) {
+    this.id = id;
   }
 }
